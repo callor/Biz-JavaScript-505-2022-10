c3c73740f3ddf87f79dddf4c050b75a6973053fb
callor <callor@callor.com>
2022-10-17 11:51:47 +0900

    배열

---
+++ JS_003/JS/main05.js
@@ -0,0 +1,10 @@
+let num1 = 30;
+document.writeln(num1);
+
+// 변수에 값 할당(저장, 대입)
+// 변수에 값을 저장하면 이전에 어떤 값이 저장되어있던지 상관없이
+// 모두 clear 되고 새롭게 저장한 값만 보관된다
+num1 = 40;
+document.writeln(num1);
+
+num1 = num1 + 10;
