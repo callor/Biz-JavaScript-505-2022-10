fbd0a50324a332dc0d214ab54629176a69e668f5
callor <callor@callor.com>
2022-10-28 17:06:53 +0900

    R

--- JS_013/www.js
+++ JS_013/www.js
@@ -5,3 +5,32 @@
  * GET /user 로 요청하면 "안녕하세요"
  * 라고 보이도록
  */
+import http from "http";
+import express from "express";
+
+const app = express();
+const server = http.createServer(app);
+server.listen(3000, "127.0.0.1", () => {
+  console.log("Server Start OK~~~");
+});
+
+app.use("/", (req, res, next) => {
+  res.setHeader("Content-Type", "text/html;charset=UTF-8");
+  next();
+});
+
+app.get("/", (req, res) => {
+  /**
+   * 여러번 text 를 보내고 싶을때는 write() 로 보내고
+   * 제일 마지막에 end() 로 마감한다
+   */
+
+  res.write("안녕하세요<hr/>");
+  res.write("오늘은 목요일<hr/>");
+  res.end("금요일이면 좋겠네<hr/>");
+});
+
+app.get("/user", (req, res) => {
+  // 한번만 보낼수 있다.
+  res.send("안녕하세요");
+});
